48403c1f7171937b74dccda34141372dd05bb97f
rikiwibowo26 <101339978+rikiwibowo26@users.noreply.github.com>
2022-03-23 21:36:07 +0700

    Create objectcar.kt

---
+++ Latihan-1/objectcar.kt
@@ -0,0 +1,20 @@
+class Car {
+  var brand = ""
+  var model = ""
+  var year = 0
+}
+
+fun main() {
+  val c1 = Car()
+  c1.brand = "Ford"
+  c1.model = "Mustang"
+  c1.year = 1969
+  
+  val c2 = Car()
+  c2.brand = "BMW"
+  c2.model = "X5"
+  c2.year = 1999
+  
+  println(c1.brand)
+  println(c2.brand)
+}
